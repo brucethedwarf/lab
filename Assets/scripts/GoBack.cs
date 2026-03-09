57f2b8ead6eff529d7684bf7e8e73f3fcb0f2b93
brucethedwarf <bruceleeshepardson@gmail.com>
2026-03-09 14:00:42 -0500

    yea more buttons let go!!!!!

---
+++ Assets/scripts/GoBack.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoBack : MonoBehaviour
+{
+    public void LoadMenu()
+
+    {
+
+        SceneManager.LoadScene("Menu");
+
+    }
+}
