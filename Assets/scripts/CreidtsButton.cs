1b0bafc0c01e5183b2306af7c59ce686a7ed73ec
brucethedwarf <bruceleeshepardson@gmail.com>
2026-04-07 13:17:10 -0500

    WE READY TO UPLOAD YEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAABBBBYYYYYYYYYYYYYYYYYYYYYYY!!!!!!!!!!!!!!

---
+++ Assets/scripts/CreidtsButton.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreidtsButton : MonoBehaviour
+{
+    public void LoadCreditsScene()
+
+    {
+
+        SceneManager.LoadScene("CreditsScene");
+
+    }
+
+}
